2add192ab6ca72f291c27c6cee0082e2bd36c03a
vu hoang nam <hoagnam150303@gmail.com>
2025-02-17 13:10:56 +0700

    fix page

---
+++ src/pages/TutorPage/DetailTutor.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const DetailsPage = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="w-11/12 mx-auto my-10">
+      <h1 className="text-2xl font-bold text-[#F18966]">
+        Chi tiết về Tutor: {id}
+      </h1>
+      <p className="text-gray-600">
+        Đây là trang chi tiết cho Tutor có ID: {id}
+      </p>
+    </div>
+  );
+};
+
+export default DetailsPage;
